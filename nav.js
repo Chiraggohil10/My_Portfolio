df6b2b82a446e9e9c8aabf68177cf3e59e33b47d
Chiraggohil10~ <gohilchirag90994@gmail.com>
2025-01-10 02:35:54 +0530

    Add responsive navigation and download button for CV

--- nav.js
+++ nav.js
@@ -1,1 +1,15 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav');
+const navLinksItems = document.querySelectorAll('.nav a');
 
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+navLinksItems.forEach(link => {
+    link.addEventListener('click', () => {
+        if (navLinks.classList.contains('active')) {
+            navLinks.classList.remove('active');
+        }
+    });
+});
