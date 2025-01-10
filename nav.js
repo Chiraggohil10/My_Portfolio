b1cd6f67c7b738244d46284e6642fd7b670bdda6
Chiraggohil10~ <gohilchirag90994@gmail.com>
2025-01-10 13:03:03 +0530

    Refactor navigation functionality to improve user experience and enhance responsiveness

--- nav.js
+++ nav.js
@@ -1,6 +1,5 @@
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav');
-const navLinksItems = document.querySelectorAll('.nav a');
 
 function toggleNav() {
     navLinks.classList.toggle('active');
@@ -9,9 +8,3 @@ function toggleNav() {
 function closeNav() {
     navLinks.classList.remove('active');
 }
-
-burger.addEventListener('click', toggleNav);
-
-navLinksItems.forEach(link => {
-    link.addEventListener('click', closeNav);
-});
