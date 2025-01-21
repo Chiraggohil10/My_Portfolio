90a9c199fa467f2b06d2faf8e0ad206444e78d14
Chiraggohil10~ <gohilchirag90994@gmail.com>
2025-01-22 01:36:57 +0530

    event update

--- nav.js
+++ nav.js
@@ -1,10 +1,18 @@
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav');
 
-function toggleNav() {
-    navLinks.classList.toggle('active');
-}
+// function toggleNav() {
+//     navLinks.classList.toggle('active');
+// }
 
-function closeNav() {
+// function closeNav() {
+//     navLinks.classList.remove('active');
+// }
+
+burger.addEventListener('click', (e) => {
+    navLinks.classList.toggle('active');
+});
+
+navLinks.addEventListener('click', (e) => {
     navLinks.classList.remove('active');
-}
+});
